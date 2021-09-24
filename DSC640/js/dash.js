ad235e8235bba4ec36869e16c3bd7ab55dee60d4
kstoneburner <61898904+kstoneburner@users.noreply.github.com>
2021-09-24 01:08:20 -0700

    Beta 1 Submission

--- DSC640/js/dash.js
+++ DSC640/js/dash.js
@@ -1,6 +1,8 @@
+load_confirm = true;
 function openCity(evt, cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
+
 
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -18,3 +20,39 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 } 
+
+
+function showHistorical(evt, id){
+  // Declare all variables
+  var i, dd_content;
+
+
+  // Get all elements with class="tabcontent" and hide them
+  dd_content = document.getElementsByClassName("dd_historical");
+  for (i = 0; i < dd_content.length; i++) {
+    dd_content[i].style.display = "none";
+  }
+
+ if (id == 'confirm_animation'){
+    thisElem = document.getElementById(id);
+    if (load_confirm) {
+      load_confirm=false;
+
+       
+      img = document.createElement("img");
+      //adds image to the image element
+      img.src="confirm_100k_history.gif";
+      //img.src="confirm_100k_thumb.gif";
+      thisElem.innerHTML = "";
+      thisElem.appendChild(img);
+      
+      
+
+    }//*** End Load Confirem
+ }//*** End Check confirm_animation
+
+ document.getElementById(id).style.display = "inline";
+
+
+
+} //*** END showHistorical
