9aa71245d545c53115951f7af9cf9f44f39c6a4f
kstoneburner <61898904+kstoneburner@users.noreply.github.com>
2021-10-27 22:18:37 -0700

    Wed PM Update

--- DSC640/wk09_10/infographic.js
+++ DSC640/wk09_10/infographic.js
@@ -15,108 +15,263 @@ function init(){
         setTimeout(() => {  init() }, 100);
         return;    
     }
-    slider.max = (Object.keys(infographic_left).length - 1) 
+
+    slider.max = (parseInt(Object.keys(infographic_left).length) + parseInt(Object.keys(infographic_right).length) ); 
+ 	slider.max -= 3;
     var output = document.getElementById("demo");
-    output.innerHTML = Object.keys(infographic_left)[slider.value];
-    index = Object.keys(infographic_left)[slider.value];
-    console.log(infographic_left[index]);
-
-    //*** Build Table Container
-    var table = document.createElement("table");
-    var top_obj = infographic_left[index];
-    var tr = document.createElement("tr");
+    index = Object.keys(infographic_left)[slider.value]
+    output.innerHTML = index;
+    output.innerHTML = "Date: " + index + "</br>Days: " +( parseInt(slider.value+1)) ; 
     
-    for (label in top_obj[(Object.keys(top_obj))[0]]){
-        var th = document.createElement("th");    
-        if (label ==  "tier"){
-            label = "Tier";
-        }
-        if (label ==  "case_100k_avg"){
-            label = "Confirmed</br>100k";
-        }
-        if (label ==  "death_100k_avg"){
-            label = "Deaths</br>100k";
-        }
 
-        if (label ==  "relative_confirm_100k"){
-            label = "Relative</br>Confirm %";
-        }
+    const info_gfk_types = ['left','right'];
 
-        if (label ==  "relative_death_100k"){
-            label = "Relative</br>Death %";
-        }
+    for (var infographic_type in info_gfk_types) {
+    	
+    	if (info_gfk_types[infographic_type] == 'left'){
+    		var info_OBJ = infographic_left;
+    	}
 
-        th.className = label;
-        th.innerHTML = label;
-
-        tr.appendChild(th);
-    }
-    table.appendChild(tr);
+		if (info_gfk_types[infographic_type] == 'right'){
+    		var info_OBJ = infographic_right;
+    	}
+    		
+    	index = Object.keys(info_OBJ)[slider.value];	
+    	
+	    //*** Build Table Container
+	    var table = document.createElement("table");
+	    var top_obj = info_OBJ[index];
 
 
-    for (row in top_obj){
-        //console.log(row);
-        //console.log(top_obj[row]);
-        var tr = document.createElement("tr");
-        tr.className = row;
+	    
 
-        for (elem in top_obj[row]) {
+		var div = document.createElement("div");
 
-            var td = document.createElement("td");
+		if (info_gfk_types[infographic_type] == 'left'){
+			div.innerHTML = "Before Surge:</br>" + Object.keys(info_OBJ)[0] + " - " + Object.keys(info_OBJ)[Object.keys(info_OBJ).length-1];
+		}
+	    
+	    if (info_gfk_types[infographic_type] == 'right'){
+			div.innerHTML = "Post Surge:</br>" + Object.keys(info_OBJ)[0] + " - " + Object.keys(info_OBJ)[Object.keys(info_OBJ).length-1];
+		}
+	    div.className = 'header';
+	    
+	    
 
-            td.className = elem;
-            td.id = row + "_" + elem;
+	    
+	    
+	    var tr = document.createElement("tr");
+	    
+	    for (label in top_obj[(Object.keys(top_obj))[0]]){
+	        var th = document.createElement("th");    
+	        if (label ==  "tier"){
+	            label = "Tier";
+	        }
+	        if (label ==  "case_100k_avg"){
+	            label = "Confirmed</br>100k";
+	        }
+	        if (label ==  "death_100k_avg"){
+	            label = "Deaths</br>100k";
+	        }
 
-            value = top_obj[row][elem];
+	        if (label ==  "relative_confirm_100k"){
+	            label = "Relative</br>Confirm %";
+	        }
 
-            if (elem == 'tier'){
-                value = value.replace("total_","")
-                value += "%"                
-            }
+	        if (label ==  "relative_death_100k"){
+	            label = "Relative</br>Death %";
+	        }
 
-            td.innerHTML = value;
+	        th.className = label;
 
-            tr.appendChild(td)
-        }//*** END each elem
+			th.innerHTML = label;
 
-        table.appendChild(tr)
+	        tr.appendChild(th);
+	    }
+	    table.appendChild(tr);
+
+
+	    for (row in top_obj){
+	        
+	        console.log(row);
+	        //console.log(top_obj[row]);
+	        var tr = document.createElement("tr");
+	        tr.className = row;
+
+	        for (elem in top_obj[row]) {
+
+	            var td = document.createElement("td");
+
+	            td.className = elem;
+	            td.id = info_gfk_types[infographic_type]+"_" +  row + "_" + elem;
+
+	            value = top_obj[row][elem];
+
+	            if (elem == 'tier'){
+	                value = value.replace("total_","");
+	                value += "%";
+
+	                
+	            }
+
+
+				if (info_gfk_types[infographic_type] == 'left'){
+			         td.innerHTML = value;
+				}
+			    
+			    if (info_gfk_types[infographic_type] == 'right'){
+			    	td.innerHTML = "_";
+			    	 if (elem == 'tier'){
+				         td.innerHTML = value;		    	 
+			    	 }
+	            	
+				}
+
+	            
+
+	            tr.appendChild(td)
+	        }//*** END each elem
+
+	        table.appendChild(tr)
 
     }//*** END each row
+    div.appendChild(table);
+	document.getElementById("table_container").appendChild(div)
+    //document.getElementById("table_container").appendChild(table);
 
 
-    document.getElementById("table_container").appendChild(table);
+    }//*** END Each Infographic type
+
 }
 
 function handleSlider() {
 
     var slider = document.getElementById("myRange");
     var output = document.getElementById("demo");
-    index = Object.keys(infographic_left)[slider.value];
-    output.innerHTML = index; // Display the default slider value
-    
-    var top_obj = infographic_left[index];
-    
-    var table = document.getElementById("table_container")
 
-    for (row in top_obj){
-        
-        for (elem in top_obj[row]){
+    if (slider.value < (Object.keys(infographic_left).length) ) {
+	    index = Object.keys(infographic_left)[slider.value];
 
-            value = top_obj[row][elem]
-            if (elem == 'tier'){
-                value = value.replace("total_","")
-                value += "%"                
-            }
+	    
+
+	    output.innerHTML = "Date: " + index + "</br>Days: " + parseInt(parseInt(slider.value)+1) ; 
+	    
+	    var top_obj = infographic_left[index];
+	    
+	    var table = document.getElementById("table_container_left")
+
+	    for (row in top_obj){
+	        
+	        for (elem in top_obj[row]){
+
+	            value = top_obj[row][elem]
+	            if (elem == 'tier'){
+	                value = value.replace("total_","")
+	                value += "%"                
+	            }
+
+	            this_DOM = document.getElementById("left_" + row + "_" + elem);
+
+	            this_DOM.innerHTML = value;
+
+	        }//*** END each Elem in row
+
+	    }//*** End Each row in top OBJ
+	
+		//*** Draw Right Table
+	    var top_obj = infographic_right[Object.keys(infographic_right)[0]];
+	    
+	    var table = document.getElementById("table_container_right");
+
+	    for (row in top_obj){
+	        
+	        for (elem in top_obj[row]){
+
+	            value = top_obj[row][elem]
+
+	            this_DOM = document.getElementById("right_" + row + "_" + elem);
+
+	            this_DOM.innerHTML = "_";
+
+	            if (elem == 'tier'){
+	                value = value.replace("total_","")
+	                value += "%"                
+		            this_DOM.innerHTML = value;
+	            }
 
 
-            this_DOM = document.getElementById(row + "_" + elem)
-            this_DOM.innerHTML = value;
-            console.log(elem)
 
-        }//*** END each Elem in row
+	        }//*** END each Elem in row
 
 
-    }//*** End Each row in top OBJ
+	    }//*** End Each row in top OBJ	    
+
+
+
+
+	}//*** END is infographic_LEFT
+	
+	else {
+
+		//*****************************
+		//*** Draw Right Table
+		//*****************************
+		index = Object.keys(infographic_right)[slider.value - ((Object.keys(infographic_left).length)-2)];
+
+	    output.innerHTML = "Date: " + index + "</br>Days: " + slider.value ; 
+	  
+	    var top_obj = infographic_right[index];
+	    
+	    var table = document.getElementById("table_container_right");
+
+	    for (row in top_obj){
+	        
+	        for (elem in top_obj[row]){
+
+	            value = top_obj[row][elem]
+	            if (elem == 'tier'){
+	                value = value.replace("total_","")
+	                value += "%"                
+	            }
+
+	            this_DOM = document.getElementById("right_" + row + "_" + elem);
+
+	            this_DOM.innerHTML = value;
+
+	        }//*** END each Elem in row
+
+	    }//*** End Each row in top OBJ	    
+
+
+		//*****************************
+		//*** Draw Left Table
+		//*****************************
+		index = Object.keys(infographic_left)[(Object.keys(infographic_left).length)-1];
+
+		var top_obj = infographic_left[index];
+
+			    var table = document.getElementById("table_container_let");
+
+	    for (row in top_obj){
+	        
+	        for (elem in top_obj[row]){
+
+	            value = top_obj[row][elem]
+	            if (elem == 'tier'){
+	                value = value.replace("total_","")
+	                value += "%"                
+	            }
+
+	            this_DOM = document.getElementById("left_" + row + "_" + elem);
+
+	            this_DOM.innerHTML = value;
+
+	        }//*** END each Elem in row
+
+	    }//*** End Each row in top OBJ	    
+
+
+	   }//*** END update right Container
 
 }//*** END handleSlider
 
